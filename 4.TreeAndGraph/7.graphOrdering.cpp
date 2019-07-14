dbd4c231f203c95cd520c9049f625af9210d496d
virtual_hope <thakd1011@naver.com>
2019-07-14 13:39:47 +0900

    complete ordering node with dependency

--- 4.TreeAndGraph/7.graphOrdering.cpp
+++ 4.TreeAndGraph/7.graphOrdering.cpp
@@ -1,62 +1,94 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-#define MAXSIZE 10
+vector<int> orderedNode;
 
-int GRAPH[MAXSIZE][MAXSIZE];
-int visited[MAXSIZE];
+void makeGraph(vector< vector<int> > &graph, int* incomeCount) {
+	char start, dest;
+	int s, d;
+	do{
+		printf("input node to node\n");
+		scanf("\n%c %c", &start, &dest);
+		s = start - 'a';
+		d = dest - 'a';
+		
+		graph[s].push_back(d);
+		incomeCount[d]++;
 
+	}while(!(s < 0 || d < 0));
+}
+
+int findZeroIncome(int* incomeCount, int* visited, int length) {
+	for(int i=0; i<length; i++) {
+		if(!visited[i] && incomeCount[i] == 0) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int nodeOrdering(vector< vector<int> > &graph, int* incomeCount, int* visited) {
+	int tempNode;
+	
+	do{
+		tempNode = findZeroIncome(incomeCount, visited, graph.size());
+		
+		if(!visited[tempNode]) {
+			visited[tempNode] = 1;
+			orderedNode.push_back(tempNode);
+		}
+
+		for(int i=0; i<graph[tempNode].size(); i++) {
+			int next = graph[tempNode][i];
+			incomeCount[next]--;
+		}
+		
+	}while(tempNode != -1);	
+
+	for(int i=0; i<graph.size(); i++) {
+		if(visited[i] == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+//TODO: make grpha_vector (2d) & incoming count array, input node to node and incoming count plus, 
 int main(const int argc, const char* argv[]) {
 	if(argc < 2) {
-		cout << "more input plz\n";
+		cout << "more input plz\n"<<endl;
 		exit(1);
 	}
 
-	int nodeCount = argc - 1;
-	char start, dest;
-	int s, d;
-	queue <int> q;
+	int nodeCnt = argc - 1;
+	vector< vector<int> > graph;
+	int* incomeCount;
+	int* visited;
+	int result = 0;
+	
+	// init
+	incomeCount = (int*)malloc(sizeof(int) * nodeCnt);
+	visited = (int*)malloc(sizeof(int) * nodeCnt);
 
-	do{
-		scanf("\n%c %c", &start, &dest);
-		
-		s = start - 'a';
-		d = dest - 'a';
+	for(int i=0; i<nodeCnt; i++) {
+		graph.push_back(vector <int> ());
+	}
+	
+	makeGraph(graph, incomeCount);
+	result = nodeOrdering(graph, incomeCount, visited);
 
-		GRAPH[s][d] = 1;
-		cout << "s = " << s << ", d = " << d<<"\n";
-	}while(s >= 0 && d >= 0);
-
-	// check all zero column
-	for(int i = 0; i < nodeCount; i++) { // col
-		bool zero = true;
-		for(int j = 0; j < nodeCount; j++) { // row
-			if(GRAPH[j][i] == 1) {
-				zero = false;
-				break;
-			}
-		}
-
-		if(zero == true) {
-			visited[i] = 1;
-			q.push(i);
+	if(result == 1) {
+		for(int i=0; i<orderedNode.size() - 1; i++) {
+			cout << (char)(orderedNode[i] + 'a') <<"\t";
 		}
 	}
-
-	while(!q.empty()) {
-		int temp = q.front();
-		cout << temp << "\t";
-		q.pop();
-
-		for(int i=0; i<nodeCount; i++) {
-			if(GRAPH[temp][i] == 1 && !visited[i]) {
-				q.push(i);
-				visited[i] = 1;
-			}
-		}
+	else {
+		cout << "Can't find Dependency\n";
 	}
+
 }
