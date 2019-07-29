d7c598c6b891a62145c90b89768f8766cf291989
virtual_hope <thakd1011@naver.com>
2019-07-29 16:19:22 +0900

    exercise 6. recursive hanoi of tower

---
+++ 8.Recursive-DynamicProgramming/6.TowerOfHanoi.cpp
@@ -0,0 +1,41 @@
+/*
+   문제 6. 하노이타워
+   전형적인 하노이타워에서는 크기가 서로 다른 N개의 원판을 세 개의 기둥 중 아무 곳으로나 옮길 수 있다.
+   초기에 원반은 크기가 맨 위에서부터 아래로 커지는 순서대로 쌓여있다.
+   그리고 이 문제에는 다음과 같은 제약조건이 있다.
+   1) 원반을 한 번에 하나만 옮길 수 있다.
+   2) 맨 위에 있는원반 하나를 다른 기둥으로 옮길 수 있다.
+   3) 원반은 자신보다 크기가 작은 디스크 위에 놓을 수 없다.
+
+   스택을 사용하여 모든 원반을 첫 번재 기둥에서 마지막 기둥으로 옮기는 프로그램을 작성하라
+ */
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> s1, s2, s3;
+
+// recursive
+const void reHanoiTower(int n, int from, int mid, int to) {
+	if(n == 1) {
+		cout << "disk 1 from " << from << " to " << to << "\n";
+		return;
+	}
+	else {
+		reHanoiTower(n - 1, from, to, mid);
+		cout << "disk " << n << " from " << from << " to " << to << "\n";
+		reHanoiTower(n - 1, mid, from, to);
+	}
+}
+
+using namespace std;
+
+int main() {
+	int N;
+	cout << "input disk num\n";
+	cin >> N;
+
+	// recursive
+	reHanoiTower(N, 1, 2, 3);
+}
